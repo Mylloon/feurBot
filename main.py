bf66d954ded220b345d7af8100ab2009e23c8364
Mylloon <kennel.anri@tutanota.com>
2021-08-05 14:55:14 +0200

    adding ' exclusion

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class Listener(StreamListener):
                     except AttributeError:
                         tweet = status.text.lower()
                     # recovery of the last "usable" word of the tweet
-                    tweetText = sub(r"https?:\/\/\S+| *\?+| *!+| *,+|-|~|\.+|…|\^+|@\S+" + f"|{emojis()}", " ", tweet) # deletion with space
+                    tweetText = sub(r"https?:\/\/\S+| *\?+| *!+| *,+|-|~|\.+|…|\^+|@\S+| *\'+" + f"|{emojis()}", " ", tweet) # deletion with space
                     tweetText = sub(r"(?<=ui)i+|(?<=na)a+(?<!n)|(?<=quoi)i+|(?<=no)o+(?<!n)|(?<=hei)i+(?<!n)|(?<=si)i+", "", tweetText) # deletion without space
                     lastWord = tweetText.split()[-1:][0]
                     if keys["VERBOSE"]:
