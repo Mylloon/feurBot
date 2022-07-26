98c532cf3775c291ca657917e91f6a1524e2cf77
Mylloon <kennel.anri@tutanota.com>
2022-07-26 22:23:33 +0200

    add a "feur" video

--- main.py
+++ main.py
@@ -241,7 +241,8 @@ if __name__ == "__main__":
             "feur (-isson)",
             "https://twitter.com/Myshawii/status/1423219684552417281/video/1",
             "feur (-isson -ictalope -diatre -uil)",
-            "https://twitter.com/Myshawii/status/1455469162202075138/video/1"
+            "https://twitter.com/Myshawii/status/1455469162202075138/video/1",
+            "https://twitter.com/Myshawii/status/1552026689101860865/video/1"
         ] + createBaseAnswers("feuse"),
         "oui": createBaseAnswers("stiti") + createBaseAnswers("fi"),
         "non": createBaseAnswers("bril"),
